a458165ba1f4cf2d5fe800ee5f088bf3b981e9b3
mayonezny <borischubuhchiev@mail.ru>
2025-04-17 13:28:57 +0300

    hotfix warnings

--- src/pages/AccountPage/ui/AccountPage.tsx
+++ src/pages/AccountPage/ui/AccountPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BackButton } from '../../../shared/BackButton';
 import { Heading } from '../../../shared/Heading';
-import { Paragraph } from '../../../shared/Paragraph';
 import './account-page.scss';
 import { AccountWidget } from '../../../widgets/AccountWidget';
 import { Button } from '../../../shared/Button';
@@ -10,18 +9,6 @@ import { logout } from '../../../redux/store/auth/authThunks';
 
 const AccountPage: React.FC = () => {
   const dispatch = useAppDispatch();
-  // const { billsData, isLoading, isFetched } = useAppSelector(
-  //   (state) => state.billsReducer,
-  // );
-
-  // const finalAmount = useMemo(() => {
-  //   return billsData.reduce((acc: number, item) => acc + item.price, 0);
-  // }, [billsData]);
-
-  // if (!billsData.length) {
-  //   return <Navigate to="/home" />;
-  // }
-  //Я ЕБЛАН, НАЧАЛ ДЕЛАТЬ СТРАНИЦУ АККАУНТА А ПОТОМ ЗАБЫЛ И НАЧАЛ ХУЯЧИТЬ СТРАНИЦУ ИСТОРИИ
   return (
     <div className="AccountPage">
       <header className="AccountPage__header">
